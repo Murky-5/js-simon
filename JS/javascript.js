d2bc5c41530887001207b4d5e8f837b6156dcd19
MirkoDD <mirkodidio111@gmail.com>
2024-12-11 19:34:02 +0100

    added:timer_also_blocked_when_it_reaches_zero-index(added_classes_to_display_the_timer)

--- JS/javascript.js
+++ JS/javascript.js
@@ -6,6 +6,7 @@
   -- una volta che il count down arriva a 0 svuoto le box e do la possibiltà all'utente di inserici i numeri che ha memorizzato
 4 -- una volta confermate le selezioni mostro all'utente un messaggio con i numeri che è riuscito a memorizzare correttamente
 */
+const countDownElm = document.querySelector(".counter")
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -16,6 +17,7 @@ for (let i = 0; i < 5; i++) {
     randomNumbers.push(getRndInteger(1, 99))
 }
 
+
 let boxed_numbers = []
 boxed_numbers.push(document.getElementById("check_number"))
 boxed_numbers.push(document.getElementById("check_number1"))
@@ -23,7 +25,21 @@ boxed_numbers.push(document.getElementById("check_number2"))
 boxed_numbers.push(document.getElementById("check_number3"))
 boxed_numbers.push(document.getElementById("check_number4"))
 
-
 for (let i = 0; i < 5; i++) {
     boxed_numbers[i].value = randomNumbers[i]
 }
+
+
+let countDown = 1
+const stop = setInterval(() => {
+    countDownElm.innerHTML = countDown
+    if (countDown == 0) {
+        clearInterval(stop)
+    }
+    countDown--
+}, 1000)
+
+
+
+
+
