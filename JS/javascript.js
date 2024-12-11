ecc9ccb730846d76cd395486bbd02f092565f665
MirkoDD <mirkodidio111@gmail.com>
2024-12-11 17:01:07 +0100

    added:file(javascript),first_idea_of_the_programm

---
+++ JS/javascript.js
@@ -0,0 +1,8 @@
+/*
+1 -- raccolgo i dati
+2 -- inserisco i numeri nelle box per i numeri randomici
+  -- memorizzo i numeri in un array
+3 -- attraverso una funzione avvio un cout-down di 30 secondi
+  -- una volta che il count down arriva a 0 svuoto le box e do la possibiltà all'utente di inserici i numeri che ha memorizzato
+4 -- una volta confermate le selezioni mostro all'utente un messaggio con i numeri che è riuscito a memorizzare correttamente
+*/
